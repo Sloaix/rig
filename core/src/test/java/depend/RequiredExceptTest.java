1a08bc9051569eb34ad02052ce207578e174f80d
lsxiao <faith.epiphone@qq.com>
2017-05-26 01:31:25 +0800

    begin depend rules test.

---
+++ core/src/test/java/depend/RequiredExceptTest.java
@@ -0,0 +1,12 @@
+package depend;
+
+/**
+ * write with Loki
+ * author:lsxiao
+ * date:2017-05-26 01:31
+ * github:https://github.com/lsxiao
+ * zhihu:https://zhihu.com/people/lsxiao
+ */
+
+public class RequiredExceptTest {
+}
