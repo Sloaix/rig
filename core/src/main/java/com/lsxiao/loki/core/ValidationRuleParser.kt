1809d773b0b9028328c6554f26c857142623bd20
lsxiao <faith.epiphone@qq.com>
2017-05-26 02:03:27 +0800

    begin to write parser

---
+++ core/src/main/java/com/lsxiao/loki/core/ValidationRuleParser.kt
@@ -0,0 +1,25 @@
+package com.lsxiao.loki.core
+
+/**
+ * write with Loki
+ * author:lsxiao
+ * date:2017-05-25 21:56
+ * github:https://github.com/lsxiao
+ * zhihu:https://zhihu.com/people/lsxiao
+ */
+
+object ValidationRuleParser {
+    fun parse(rules: String) {
+
+    }
+
+    fun splitRules(rules: String) {
+
+        //required|min:2|max:5
+        val split = rules.split("|")
+    }
+
+    fun parseStringRule(rules: String) {
+
+    }
+}
