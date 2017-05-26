183f05c0a6b87aed04562c3ac1b997d439854d82
lsxiao <faith.epiphone@qq.com>
2017-05-26 23:23:31 +0800

    refactor

---
+++ core/src/main/java/com/lsxiao/loki/core/ValidateResult.kt
@@ -0,0 +1,19 @@
+package com.lsxiao.loki.core
+
+import com.lsxiao.loki.core.rule.LokiRule
+
+/**
+ * write with Loki
+ * author:lsxiao
+ * date:2017-05-26 20:59
+ * github:https://github.com/lsxiao
+ * zhihu:https://zhihu.com/people/lsxiao
+ */
+
+class ValidateResult(val errors: Map<LokiRule, String> = emptyMap()) {
+    val isFail: Boolean
+        get() = !isPass
+
+    val isPass: Boolean
+        get() = errors.isEmpty()
+}
