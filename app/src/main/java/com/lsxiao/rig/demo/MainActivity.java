52a7df5e3c655b9a15bc2e7651ca7f2f3869f50d
xiaolishang <faith.epiphone@qq.com>
2017-06-09 16:13:45 +0800

    demo

--- app/src/main/java/com/lsxiao/rig/demo/MainActivity.java
+++ app/src/main/java/com/lsxiao/rig/demo/MainActivity.java
@@ -1,42 +1,8 @@
 package com.lsxiao.rig.demo;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import com.lsxiao.rig.core.Rig;
-import com.lsxiao.rig.core.annotation.Rule;
-
-import butterknife.BindView;
-import butterknife.OnClick;
 
 public class MainActivity extends BaseActivity {
-
-    @Rule(value = "int|min_len:6|max_len:12", name = "密码")
-    public String password = "123456";
-
-    @BindView(R.id.tv_message)
-    public TextView mTvMessage;
-
-    @Rule(value = "when_filled:密码|int|min_len:6|max_len:12|same_as:密码", name = "确认密码")
-    @BindView(R.id.edit_address)
-    public EditText mEditContent;
-
-    @OnClick(R.id.btn_check)
-    public void onClick() {
-        Log.d("MainActivity", "Rig.check(this):" + Rig.check(this));
-        if (Rig.check(this).isPass()) {
-            mTvMessage.setText("校验通过");
-        } else {
-            mTvMessage.setText(String.format("校验未通过\n%s", Rig.check(this)));
-        }
-    }
-
-    @Rule("filled")
-    public String getCity() {
-        return "NewYork";
-    }
 
     @Override
     void afterCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/lsxiao/rig/demo/SimpleRuleActivity.java
@@ -0,0 +1,16 @@
+package com.lsxiao.rig.demo;
+
+import android.os.Bundle;
+
+public class SimpleRuleActivity extends BaseActivity {
+
+    @Override
+    int getLayoutId() {
+        return R.layout.activity_simple_rule;
+    }
+
+    @Override
+    void afterCreate(Bundle savedInstanceState) {
+
+    }
+}
